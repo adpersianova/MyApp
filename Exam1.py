fd83ba3745b417624f7324902da40e15b3d1bc2d
adpersianova <99672462+adpersianova@users.noreply.github.com>
2022-02-18 21:49:01 +0300

    Update Exam1.py

--- Exam1.py
+++ Exam1.py
@@ -5,9 +5,9 @@ D = p*p - 4*p*d
 if D>0:
     x1 = (-a + D**(1/2))/(2*p)
     x2 = (-a - D**(1/2))/(2*p)
-    print ("Первый корень ",round(x1), " второй корень ",round(x2))
+    print ("the first root ",round(x1), " the second root ",round(x2))
 elif D==0:
-    x = (-b)/(2*a)
-    print("корень: ",x)
+    x = (-a)/(2*p)
+    print("root: ",x)
 else:
-    print("нет корней")
+    print("no roots")
